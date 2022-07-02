56b427c3d6a6dce0bd90603e531ec2f0d649c6a3
kenmaready <ken@maready.com>
2022-07-02 07:51:28 -0400

    Weather data (from search location) fetch and push info to console working, beginning to implement fetch of user location

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        dimUndim(button: sender)
+        playSound(note: sender.currentTitle)
+    }
+
+    
+    func dimUndim(button: UIButton) {
+        button.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            // Put your code which should be executed with a delay here
+            button.alpha = 1.0
+        }
+    }
+    
+    func playSound(note: String?) {
+        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else {
+            print("url not found")
+            return
+        }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: "wav")
+            player!.play()
+            
+        } catch let error as NSError {
+            print("error: \(error.localizedDescription)")
+        }
+    }
     
 
 }
